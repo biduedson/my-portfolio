4304274fce0c7f58388f17b599c26b46121ccf72
biduedson <112134265+biduedson@users.noreply.github.com>
2023-03-06 22:08:51 -0300

    inserindo links das redes sociais

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -3,9 +3,9 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa'
 import "../styles/components/socialnetworks.sass";
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub /> },
-    { name: "instagran", icon: <FaInstagram /> }
+    { name: "linkedin", icon: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/edson-gomes-5307b21a2/' },
+    { name: "github", icon: <FaGithub />, link: 'https://github.com/biduedson' },
+    { name: "instagran", icon: <FaInstagram />, link: 'https://www.instagram.com/biduedson/' }
 
 ]
 
@@ -13,7 +13,7 @@ function SocialNetworks() {
     return (
         <section id='social-networks'>
             {socialNetworks.map((network) => (
-                <a href="#" className='social-btn' id={network.name} key={network.name}>
+                <a href={network.link} className='social-btn' id={network.name} key={network.name}>
                     {network.icon}
                 </a>
             ))}
